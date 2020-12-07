5ac0c3a66ea03c8f28d5411fa2934cc5334c8606
Matthew Olson <molson194@gmail.com>
2020-12-06 20:53:31 -0800

    Auth remote issues (1)

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 Action Items
+# Fix Authentication prod api
 # Create new function to (a) retrieve linked in token, (b) get email, uid, name, (c) create/update user with new JWT, (d) return token to user
+
+Advice for Static Web App Team
+* Function logs by default (create AppInsights and link)
--- api/Authentication/__init__.py
+++ api/Authentication/__init__.py
@@ -7,13 +7,13 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    code = req.params.get('code')
-    print(code)
-    payload = {'grant_type': 'authorization_code', 'code': code, 'client_id':'863gl16an25sq9', 'client_secret':os.environ["LINKEDIN_CLIENT_SECRET"], 'redirect_uri':os.environ["LINKEDIN_REDIRECT_URI"]}
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    r = requests.post("https://www.linkedin.com/oauth/v2/accessToken", headers=headers, data=payload)
-    print(r.content)
+    #code = req.params.get('code')
+    #print(code)
+    #payload = {'grant_type': 'authorization_code', 'code': code, 'client_id':'863gl16an25sq9', 'client_secret':os.environ["LINKEDIN_CLIENT_SECRET"], 'redirect_uri':os.environ["LINKEDIN_REDIRECT_URI"]}
+    #headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    #r = requests.post("https://www.linkedin.com/oauth/v2/accessToken", headers=headers, data=payload)
+    #print(r.content)
     return func.HttpResponse(
-            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            "This HTTP triggered function executed successfully.",
             status_code=200
     )
