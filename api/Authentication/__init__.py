89ab061f7991a3f76695e66d8a9c4e0d7ceb44d5
Matthew Olson <molson194@gmail.com>
2020-12-06 21:27:54 -0800

    Auth remote issues (4)

--- api/Authentication/__init__.py
+++ api/Authentication/__init__.py
@@ -7,7 +7,7 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
   logging.info('Python HTTP trigger function processed a request.')
 
-  token = req.params.get('token')
+  token = req.route_params.get('token')
   print(token)
   payload = {'grant_type': 'authorization_code', 'code': token, 'client_id':'863gl16an25sq9', 'client_secret':os.environ["LINKEDIN_CLIENT_SECRET"], 'redirect_uri':os.environ["LINKEDIN_REDIRECT_URI"]}
   headers = {'Content-Type': 'application/x-www-form-urlencoded'}
