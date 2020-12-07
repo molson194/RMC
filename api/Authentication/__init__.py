0f2f1f8f726f840d544ec5103813aacab8207ad9
Matthew Olson <molson194@gmail.com>
2020-12-06 22:23:03 -0800

    Auth remote issues (7)

--- api/Authentication/__init__.py
+++ api/Authentication/__init__.py
@@ -1,6 +1,6 @@
 import logging
-import os
-import requests
+#import os
+#import requests
 import azure.functions as func
 
 
@@ -9,11 +9,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
   token = req.params.get('token')
   print(token)
-  payload = {'grant_type': 'authorization_code', 'code': token, 'client_id':'863gl16an25sq9', 'client_secret':os.environ["LINKEDIN_CLIENT_SECRET"], 'redirect_uri':os.environ["LINKEDIN_REDIRECT_URI"]}
-  headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-  r = requests.post("https://www.linkedin.com/oauth/v2/accessToken", headers=headers, data=payload)
-  print(r.content)
+  #payload = {'grant_type': 'authorization_code', 'code': token, 'client_id':'863gl16an25sq9', 'client_secret':os.environ["LINKEDIN_CLIENT_SECRET"], 'redirect_uri':os.environ["LINKEDIN_REDIRECT_URI"]}
+  #headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+  #r = requests.post("https://www.linkedin.com/oauth/v2/accessToken", headers=headers, data=payload)
+  #print(r.content)
   return func.HttpResponse(
-    "This HTTP triggered function executed successfully.",
+    "This HTTP triggered function executed successfully. " + token,
     status_code=200
   )
