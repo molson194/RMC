12281e1f3b21ef324a89de69fd967834dee78073
Matthew Olson <molson194@gmail.com>
2020-12-06 21:20:47 -0800

    Auth remote issues (3)

--- api/Authentication/__init__.py
+++ api/Authentication/__init__.py
@@ -1,18 +1,18 @@
 import logging
-#import os
-#import requests
+import os
+import requests
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
   logging.info('Python HTTP trigger function processed a request.')
 
-  #code = req.params.get('code')
-  #print(code)
-  #payload = {'grant_type': 'authorization_code', 'code': code, 'client_id':'863gl16an25sq9', 'client_secret':os.environ["LINKEDIN_CLIENT_SECRET"], 'redirect_uri':os.environ["LINKEDIN_REDIRECT_URI"]}
-  #headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-  #r = requests.post("https://www.linkedin.com/oauth/v2/accessToken", headers=headers, data=payload)
-  #print(r.content)
+  token = req.params.get('token')
+  print(token)
+  payload = {'grant_type': 'authorization_code', 'code': token, 'client_id':'863gl16an25sq9', 'client_secret':os.environ["LINKEDIN_CLIENT_SECRET"], 'redirect_uri':os.environ["LINKEDIN_REDIRECT_URI"]}
+  headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+  r = requests.post("https://www.linkedin.com/oauth/v2/accessToken", headers=headers, data=payload)
+  print(r.content)
   return func.HttpResponse(
     "This HTTP triggered function executed successfully.",
     status_code=200
--- public/callback/main.js
+++ public/callback/main.js
@@ -1,11 +1,11 @@
 const queryString = window.location.search;
 console.log(queryString);
 const urlParams = new URLSearchParams(queryString);
-const code = urlParams.get('code')
-const state = urlParams.get('state')
+const code = urlParams.get('code');
+const state = urlParams.get('state');
 
 (async function () {
-  let response = await fetch('/api/Authentication?code=' + code);
+  let response = await fetch('/api/Authentication?token=' + code);
   let message = await response.text();
   console.log(message);
 })();
